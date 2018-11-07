7a04412b0f86f4a59f701957f91f8d1892344c36
ptkmc <hi@pmcpherson.net>
2018-11-07 00:17:33 -0600

    Added static front end files and included jquery

--- index.js
+++ index.js
@@ -7,9 +7,11 @@ const todoRoutes = require('./routes/todos');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
+app.use(express.static(__dirname + '/views'));
 
 app.get('/', function(req, res){
-    res.send('hello from the root route');
+    res.sendFile('index.html');
 });
 
 app.use('/api/todos/', todoRoutes);

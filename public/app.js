cb8cb8c25c7bb8b1913374a61638ce94246a48f6
ptkmc <hi@pmcpherson.net>
2018-11-07 23:59:28 -0600

    Added initial AJAX call

--- public/app.js
+++ public/app.js
@@ -1,1 +1,12 @@
-alert('hi from app.js');
+$(document).ready(function(){
+    $.getJSON('api/todos')
+    .then(addTodos);
+});
+
+
+function addTodos(todos) {
+    todos.forEach(function(todo){
+        const newTodo = $('<li class="task">' + todo.name + '</li>');
+        $('.list').append(newTodo);
+    });
+}
